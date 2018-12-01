b3ae6a5beeff491732c52553e3751a85436f69f0
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2018-12-01 15:37:34 -0800

    Change exploratory visualization chart to use percentages

--- src/exploratory_visualization.R
+++ src/exploratory_visualization.R
@@ -18,16 +18,15 @@ input_data = read_csv(input_file)
 
 # make Marital and CarInsurance a factor
 input_data <- input_data %>% 
-  mutate(CarInsurance = factor(CarInsurance, levels = c(0, 1),labels = c("Offer Refused", "Bought")), Marital = factor(Marital))
+  mutate(CarInsurance = factor(CarInsurance, levels = c(0, 1), labels = c("Offer Refused", "Bought")), Marital = factor(Marital))
 
 # visualize the data
 input_data %>%
   ggplot(aes(x = CarInsurance, fill = CarInsurance)) +
-  geom_bar(stat = "count", position = position_dodge(), show.legend = FALSE) +
-  geom_text(stat = "count", aes(label = ..count..), vjust = -0.4, colour = "black") +
-  facet_wrap(~Marital) +
-  labs(x = "CarInsurance",
-       y = "Value") +
+  geom_bar(aes(y = (..count..)/sum(..count..)), position = position_dodge(), show.legend = FALSE) +
+  geom_text(aes(y = ((..count..)/sum(..count..)), label = scales::percent((..count..)/sum(..count..))), stat = "count", vjust = -0.25) +  facet_wrap(~Marital) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "CarInsurance", y = "Percent") +
   theme_bw() +
   ggtitle("Relationship between car insurance purchase and marital status") +
   theme(plot.title = element_text(hjust = 0.5))
